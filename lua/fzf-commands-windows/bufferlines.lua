46b9e1d54cd87afcaa7a230f6ca12337c47990e7
cpkio <krasilnikov.cpkio@gmail.com>
2022-11-24 14:56:55 +0300

    Bufferlines send multiple selections to QfList on Enter and LocList on Alt-Enter

--- lua/fzf-commands-windows/bufferlines.lua
+++ lua/fzf-commands-windows/bufferlines.lua
@@ -5,7 +5,7 @@ local fn, api = utils.helpers()
 return function(options)
   coroutine.wrap(function()
     options = utils.normalize_opts(options)
-    local opts = ('--header-lines=0 --ansi --prompt="BLines> "')
+    local opts = ('--reverse --header-lines=0 --multi --expect=alt-enter --ansi --prompt="BLines> "')
     local items = {}
 
     local buflines = api.buf_get_lines(0,0,-1,0)
@@ -22,13 +22,36 @@ return function(options)
       return
     end
 
-    local cmd
-    local linenum, _ = string.match(lines[1], '^%s*(%d+)')
-    print(linenum)
-    api.command(tostring(linenum))
-
--- TODO
--- 3) pass multiple lines to quickfix list
+    if lines[1] == "" then
+      if #lines == 2 then
+        local linenum, _ = string.match(lines[1], '^%s*(%d+)')
+        api.command(linenum)
+      else
+        local bufnum = api.get_current_buf()
+        local itemsqf = {}
+        for j = 2, #lines do
+          local linenum, line = string.match(lines[j], '^%s*(%d+)%s*(%S.+)')
+          table.insert(itemsqf, { bufnr = bufnum, lnum = tonumber(linenum), text = line })
+        end
+        fn.setqflist({},'r',{ id = 'FzfBLines', items = itemsqf, title = 'FzfBLines'})
+        api.command('copen')
+      end
+    end
+    if lines[1] == "alt-enter" then
+      if #lines == 2 then
+        local linenum, _ = string.match(lines[1], '^%s*(%d+)')
+        api.command(linenum)
+      else
+        local bufnum = api.get_current_buf()
+        local itemsqf = {}
+        for j = 2, #lines do
+          local linenum, line = string.match(lines[j], '^%s*(%d+)%s*(%S.+)')
+          table.insert(itemsqf, { bufnr = bufnum, lnum = tonumber(linenum), text = line })
+        end
+        fn.setloclist(fn.win_getid(),{},'r',{ id = 'FzfBLines', items = itemsqf, title = 'FzfBLines'})
+        api.command('lopen')
+      end
+    end
 
   end)()
 end
