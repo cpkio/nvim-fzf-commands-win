81b8fa2f6e5b513226f0a0159d4d358cf56dccb5
cpkio <krasilnikov.cpkio@gmail.com>
2022-12-05 23:53:11 +0300

    Tweaking GDiff output (.gitconfig [delta] needed)

--- lua/fzf-commands-windows/gdiff.lua
+++ lua/fzf-commands-windows/gdiff.lua
@@ -10,29 +10,29 @@ return function(opts)
   coroutine.wrap(function ()
     local fname = fn.fnamemodify(api.buf_get_name(0), ":t") -- барахлит функция на многооконных раскладках, но ладно
     local gitsrc = "git log --all --pretty=format:%H" .. d_delimiter .."%s" .. d_delimiter .. "%d -- **/".. fname
-    local fzfpreview = "git diff {1} -- **/" .. fname .. " | delta --wrap-max-lines=0" ..
-    ' --file-style=white                               ' ..
+    local fzfpreview = "git diff {1} -- **/" .. fname .. " | delta --wrap-max-lines=unlimited " ..
+    '--file-style=white                               ' ..
 
-    ' --minus-style=brightred                          ' ..
-    ' --minus-non-emph-style=brightred                 ' ..
-    ' --minus-emph-style=brightyellow                  ' ..
-    ' --minus-empty-line-marker-style=brightred        ' ..
+    '--minus-style=brightred                          ' ..
+    '--minus-non-emph-style=brightred                 ' ..
+    '--minus-emph-style=brightyellow                  ' ..
+    '--minus-empty-line-marker-style=brightred        ' ..
 
-    ' --zero-style=white                               ' ..
+    '--zero-style=white                               ' ..
 
-    ' --plus-style=yellow                              ' ..
-    ' --plus-non-emph-style=yellow                     ' ..
-    ' --plus-emph-style=brightyellow                   ' ..
-    ' --plus-empty-line-marker-style=yellow            ' ..
+    '--plus-style=yellow                              ' ..
+    '--plus-non-emph-style=yellow                     ' ..
+    '--plus-emph-style=brightyellow                   ' ..
+    '--plus-empty-line-marker-style=yellow            ' ..
 
- -- ' --grep-file-style=blue                           ' ..
- -- ' --grep-line-number-style=blue                    ' ..
- -- ' --whitespace-error-style=reverse brightblue      ' ..
+ -- '--grep-file-style=blue                           ' ..
+ -- '--grep-line-number-style=blue                    ' ..
+ -- '--whitespace-error-style=reverse brightblue      ' ..
 
-    ' --line-numbers-minus-style=brightred             ' ..
-    ' --line-numbers-zero-style=white                  ' ..
-    ' --line-numbers-plus-style=yellow                 ' ..
-    ' --width=%FZF_PREVIEW_COLUMNS%'
+    '--line-numbers-minus-style=brightred             ' ..
+    '--line-numbers-zero-style=white                  ' ..
+    '--line-numbers-plus-style=yellow                 ' ..
+    '--width=%FZF_PREVIEW_COLUMNS%'
     local fzfcommand = '--prompt="GDiff> " --delimiter=' .. d_delimiter .. ' --preview-window=up:border-none --preview=' .. fn.shellescape(fzfpreview)
     local choices = opts.fzf(gitsrc, fzfcommand) -- проблема в fzfcommand
     if not choices then return end
