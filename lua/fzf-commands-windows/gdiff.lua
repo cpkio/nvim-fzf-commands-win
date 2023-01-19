a5290e37a9130b1e895452c27b2b0a66f282a7f8
cpkio <krasilnikov.cpkio@gmail.com>
2023-01-19 12:42:45 +0300

    Gdiff now loads any other version of a currently edited file to vsplit tempbuffer

--- lua/fzf-commands-windows/gdiff.lua
+++ lua/fzf-commands-windows/gdiff.lua
@@ -5,7 +5,27 @@ local action = require "fzf.actions".action
 local fn, api = utils.helpers()
 
 local d_delimiter = ' '
+
 return function(opts)
+  local buffer_number = -1
+
+  local function log(_, data)
+    if data then
+      local r = {}
+      for _, v in ipairs(data) do
+        local w, _ = v:gsub('\r', '')
+        table.insert(r, w)
+      end
+      vim.api.nvim_buf_set_lines(buffer_number, -1, -1, true, r)
+    end
+  end
+
+  local function open_buffer()
+    if buffer_number == -1 then
+      vim.api.nvim_command('botright vnew')
+      buffer_number = vim.api.nvim_get_current_buf()
+    end
+  end
   opts = utils.normalize_opts(opts)
   coroutine.wrap(function ()
     local fname = fn.fnamemodify(api.buf_get_name(0), ":t") -- барахлит функция на многооконных раскладках, но ладно
@@ -36,5 +56,15 @@ return function(opts)
     local fzfcommand = term.fzf_colors .. '--prompt="GDiff> " --delimiter=' .. d_delimiter .. ' --preview-window=up:border-none --preview=' .. fn.shellescape(fzfpreview)
     local choices = opts.fzf(gitsrc, fzfcommand) -- проблема в fzfcommand
     if not choices then return end
+    local s = vim.split(choices[1], d_delimiter)
+    local fnm = './' .. vim.fn.expand('%'):gsub('\\','/')
+    open_buffer()
+    vim.fn.jobstart({'git', 'show', '-t', s[1]..':'..fnm }, {
+      stdout_buffered = true,
+      overlapped = true,
+      on_stdout = log,
+      on_stderr = log
+    })
+
   end)()
 end
