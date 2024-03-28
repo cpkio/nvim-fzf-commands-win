2f37cd021e1a34e0521673e03688b32a9771e861
cpkio <krasilnikov.cpkio@gmail.com>
2024-03-28 14:43:15 +0300

    Utility functions

--- lua/fzf-commands-windows/utils.lua
+++ lua/fzf-commands-windows/utils.lua
@@ -1,3 +1,4 @@
+local utf = require'lua-utf8'
 local M = {}
 
 M.delim = ' '
@@ -21,6 +22,22 @@ function M.normalize_opts(opts)
   if not opts then opts = {} end
   if not opts.fzf then opts.fzf = require"fzf".fzf end
   return opts
+end
+
+function M.unnil(c)
+  if not c or c == vim.NIL then
+    return '‧'
+  end
+  return c
+end
+
+function M.pad(content, len)
+  local content = M.unnil(content)
+  local pad = len - utf.len(content)
+  if pad <= 0 then return content end
+  local pre = pad / 2
+  local post = pad / 2 + pad % 2
+  return string.rep(' ', pre) .. content .. string.rep(' ', post)
 end
 
 return M
