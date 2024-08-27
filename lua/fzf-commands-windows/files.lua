f215853731dcdc7d0a340f1a527b3d6dcd6e21eb
cpkio <krasilnikov.cpkio@gmail.com>
2024-08-27 09:40:30 +0300

    Prevent `Files` command from showing hidden files, even if no `.git` exists

--- lua/fzf-commands-windows/files.lua
+++ lua/fzf-commands-windows/files.lua
@@ -7,7 +7,7 @@ return function(opts)
   opts = utils.normalize_opts(opts)
   local command
   if fn.executable("fd") == 1 then
-    command = "fd --no-ignore --color never -tf -L --strip-cwd-prefix"
+    command = "fd --no-require-git --color never -tf -L --strip-cwd-prefix"
   end
 
   local preview
