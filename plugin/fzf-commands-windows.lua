3a7bc287dda2949b213638bc3767311604fd1326
cpkio <krasilnikov.cpkio@gmail.com>
2025-06-06 17:05:51 +0300

    Added explicit commands for some modules

--- plugin/fzf-commands-windows.lua
+++ plugin/fzf-commands-windows.lua
@@ -53,3 +53,94 @@ vim.api.nvim_create_user_command('GitBranch',
   end,
   {}
 )
+vim.api.nvim_create_user_command('Files',
+  function(opts)
+    require("fzf-commands-windows").files({
+      fzf = fzfcmd }, opts.args)
+  end,
+  {}
+)
+vim.api.nvim_create_user_command('FilesHidden',
+  function(opts)
+    require("fzf-commands-windows").files({
+      fzf = fzfcmd, extra = {'--hidden', '--no-ignore'} }, opts.args)
+  end,
+  {}
+)
+vim.api.nvim_create_user_command('Lines',
+  function(opts)
+    require("fzf-commands-windows").lines({
+      fzf = fzfcmd }, opts.args)
+  end,
+  {}
+)
+vim.api.nvim_create_user_command('BLines',
+  function(opts)
+    require("fzf-commands-windows").bufferlines({
+      fzf = fzfcmd }, opts.args)
+  end,
+  {}
+)
+vim.api.nvim_create_user_command('Marks',
+  function(opts)
+    require("fzf-commands-windows").marks({
+      fzf = fzfcmd }, opts.args)
+  end,
+  {}
+)
+vim.api.nvim_create_user_command('Buffers',
+  function(opts)
+    require("fzf-commands-windows").buffers({
+      fzf = fzfcmd }, opts.args)
+  end,
+  {}
+)
+vim.api.nvim_create_user_command('Registers',
+  function(opts)
+    require("fzf-commands-windows").registers({
+      fzf = fzfcmd }, opts.args)
+  end,
+  {}
+)
+vim.api.nvim_create_user_command('FileTypes',
+  function(opts)
+    require("fzf-commands-windows").filetypes({
+      fzf = fzfcmd }, opts.args)
+  end,
+  {}
+)
+vim.api.nvim_create_user_command('CommandsHistory',
+  function(opts)
+    require("fzf-commands-windows").commands_history({
+      fzf = fzfcmd }, opts.args)
+  end,
+  {}
+)
+vim.api.nvim_create_user_command('SearchesHistory',
+  function(opts)
+    require("fzf-commands-windows").search_history({
+      fzf = fzfcmd }, opts.args)
+  end,
+  {}
+)
+vim.api.nvim_create_user_command('Directories',
+  function(opts)
+    require("fzf-commands-windows").directories({
+      fzf = fzfcmd }, opts.args)
+  end,
+  {}
+)
+vim.api.nvim_create_user_command('GitFiles',
+  function(opts)
+    require("fzf-commands-windows").gitfiles({
+      fzf = fzfcmd }, opts.args)
+  end,
+  {}
+)
+vim.api.nvim_create_user_command('GitFilesLastCommitted',
+  function(opts)
+    require("fzf-commands-windows").gitfileslastcommitted({
+      fzf = fzfcmd }, opts.args)
+  end,
+  {}
+)
